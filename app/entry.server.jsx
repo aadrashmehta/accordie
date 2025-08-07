abc800dd0b1d459842c350870cd34016ec5b254d
Aadrash Mehta <mehta.aadrash05@gmail.com>
2025-08-08 02:45:57 +0530

    Updated app/entry.server.jsx

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -14,6 +14,11 @@ export default async function handleRequest(
   remixContext,
 ) {
   addDocumentResponseHeaders(request, responseHeaders);
+
+  // Add headers to allow iframe embedding in Shopify
+  responseHeaders.set("X-Frame-Options", "ALLOWALL");
+  responseHeaders.set("Content-Security-Policy", "frame-ancestors *");
+
   const userAgent = request.headers.get("user-agent");
   const callbackName = isbot(userAgent ?? "") ? "onAllReady" : "onShellReady";
 
